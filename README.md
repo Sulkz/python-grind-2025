ed7218282d965671ee999be33448f526becf4ba9
Reuel Igbokwe <reueligbokwe@Reuels-MacBook-Air.local>
2025-06-09 01:45:00 +0100

    Solved twosum in python using brute force tomorrow this will be optimised to solve in o(1) time using dictionaries

---
+++ README.md
@@ -0,0 +1,5 @@
+# Python Grind 2025
+
+LeetCode + technical interview prep for high-impact software engineering roles (JPMorgan, Mako, Citadel, Bloomberg). 
+
+✅ Goal: Master Python + DS&A by September 2025.
---
+++ easy/two_sum.py
@@ -0,0 +1,20 @@
+"""
+Problem: Two Sum
+Difficulty: Easy
+Approach 1: Brute Force (O(n^2))
+Approach 2: Hash Map (O(n))
+
+LeetCode link: https://leetcode.com/problems/two-sum/
+"""
+
+def two_sum(nums, target):
+    # Brute force
+    for x in range(0, len(nums)):
+        for y in range(x+1,len(nums)):
+            if(nums[x] +nums[y] == target):
+                return [x,y]
+    return[]
+
+def two_sum_optimized(nums, target):
+    # Hashmap
+    ...
