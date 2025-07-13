9679d89f06bcd2ed51463838faeb942a18aea9bb
Reuel Igbokwe <reueligbokwe@Reuels-MacBook-Air.local>
2025-07-13 17:18:30 +0100

    went throught the process of solving the longest subarray string without repeating this required using sets and a sliding window with a left pointer and right used in the for loop removing the leftmost characters to reduce the window size still in the learning phase.

---
+++ medium/longest_subarray_String_without repeating.py
@@ -0,0 +1,18 @@
+"""
+Requires sliding window with one pointer that needs to be moved manually utilising sets as well
+"""
+class Solution(object):
+    def lengthOfLongestSubstring(s):
+        memo = set()
+        max_len = 0 
+        lp = 0
+        for rp in range(len(s)):
+            while s[rp] in memo:
+                #if the letter is already in the set then we reduce window but removing character at position lp
+                memo.remove(s[lp])
+                lp += 1
+            memo.add(s[rp])
+            #finding the lkenght of the subarray window
+            max_len = max(max_len, rp -lp + 1)
+        return max_len
+    
