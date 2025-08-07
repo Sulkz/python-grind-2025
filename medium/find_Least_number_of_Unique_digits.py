d196746f1371a1d872c1588d591f08059375a8f0
Reuel Igbokwe <reueligbokwe@Mac.lan>
2025-08-07 16:06:46 +0100

    accidentally wrote a problem into my notes section this is a greedy algorithm

---
+++ medium/find_Least_number_of_Unique_digits.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+
+class Solution(object):
+    def findLeastNumOfUniqueInts(arr,k):
+        cnt = Counter(arr)
+        freq = sorted(cnt.values())
+        for i in range(len(freq)):
+            if k >= freq[i]:
+                k -= freq[i]
+                freq[i] = 0
+            else:
+                freq[i] -= k
+                k = 0
+                break
+        return sum(1 for f in freq if f > 0)
+                
+    
+        
+print(Solution.findLeastNumOfUniqueInts([5,5,4],1))
--- notes/type.py
+++ notes/type.py
@@ -1,20 +1,28 @@
-from collections import Counter
+# from collections import Counter
 
 
-class Solution(object):
-    def findLeastNumOfUniqueInts(arr,k):
-        cnt = Counter(arr)
-        freq = sorted(cnt.keys())
-        for i in range(len(freq)):
-            if k >= freq[i]:
-                k -= freq[i]
-                freq[i] = 0
-            else:
-                freq[i] -= k
-                k = 0
-                break
-        return sum(1 for f in freq if f > 0)
+# class Solution(object):
+#     def findLeastNumOfUniqueInts(arr,k):
+#         cnt = Counter(arr)
+#         freq = sorted(cnt.keys())
+#         for i in range(len(freq)):
+#             if k >= freq[i]:
+#                 k -= freq[i]
+#                 freq[i] = 0
+#             else:
+#                 freq[i] -= k
+#                 k = 0
+#                 break
+#         return sum(1 for f in freq if f > 0)
                 
     
         
-print(Solution.findLeastNumOfUniqueInts([5,5,4],1))
+# print(Solution.findLeastNumOfUniqueInts([5,5,4],1))
+
+
+coin = [1,2,3]
+
+coin.reverse()
+print(coin)
+
+
